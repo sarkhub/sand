0df528e52320ac43960d042ffbaf488f77331285
sarkhub <57264451+sarkhub@users.noreply.github.com>
2019-12-12 13:17:06 -0600

    Add files via upload

--- alpha_vantage_ticker_pull_export.py
+++ alpha_vantage_ticker_pull_export.py
@@ -3,16 +3,25 @@ from alpha_vantage.techindicators import TechIndicators
 #from matplotlib.pyplot import figure
 #import matplotlib.pyplot as plt
 import pandas as pd
+import os 
+
+os.chdir(r"C:\tmp")
 
 # Your key here
 key = 'key'
+
+
+# ticker we are wanting to capture data on
+ticker_symbol = 'ORCL'
+
+
 # Chose your output format, or default to JSON (python dict)
 ts = TimeSeries(key, output_format='pandas')
 ti = TechIndicators(key)
 
 # Get the data, returns a tuple
 # aapl_data is a pandas dataframe, aapl_meta_data is a dict
-tick_data, tick_meta_data = ts.get_daily(symbol='ORCL')
+tick_data, tick_meta_data = ts.get_daily(symbol=ticker_symbol)
 
 
 
@@ -29,10 +38,13 @@ tick_data, tick_meta_data = ts.get_daily(symbol='ORCL')
 stock_df = pd.DataFrame.from_dict(tick_data)
 
 # add symbol column and poplulate
-stock_df['symbol'] = 'ORCL'
+stock_df['symbol'] = ticker_symbol
 
 # see contents with new column and ticker symbol
 #stock_df
 
 # export to csv
-export_csv = stock_df.to_csv (r'C:\tmp\export_dataframe.csv', index = True, header=True) 
+export_csv = stock_df.to_csv (r'file_out.csv', index = True, header=True) 
+
+# change generic file name to that of the ticker
+os.rename('file_out.csv', ticker_symbol+'.csv')
